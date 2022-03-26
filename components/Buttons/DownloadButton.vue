a586cd0d9e9f8163f072ee1750afd591596fa4da
yeonwoochoi <chldusdn20@gmail.com>
2022-03-27 01:12:43 +0900

    feat: add donwload button

---
+++ components/Buttons/DownloadButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    type="button"
+    class="no-background-hover elevation-0 mx-2 pb-2 subtitle-2 font-weight-bold"
+    :onclick="`window.open('http://${link}', '_blank')`"
+    formtarget="_blank"
+    style="background-color: transparent; color: #666666; height: fit-content"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "DownloadButton",
+  props: {
+    link: {
+      type: String,
+      default: () => ''
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

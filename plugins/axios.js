78ac36538cefad8f174821d861201012aff9f633
beagle <chldusdn20@gmail.com>
2022-04-25 19:55:04 +0900

    refactor: set refresh token header during request refresh token

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,8 @@
-export default ({ $axios, store }, inject) => {
+export default ({ $axios, app, error: nuxtError }, inject) => {
   $axios.onRequest(config => {
+    if (config.url === '/user/refresh-token') {
+      config.headers['Authorization'] = 'Bearer ' + app.$auth.strategy.refreshToken.get()
+    }
     console.log('Making request to ' + config.url)
   })
 

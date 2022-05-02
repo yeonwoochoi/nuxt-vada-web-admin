6709fc7699ad2d72a287258e80ea8af6aa0685cf
beagle <chldusdn20@gmail.com>
2022-05-02 12:07:32 +0900

    feat: add api key read and update

--- pages/data/index.vue
+++ pages/data/index.vue
@@ -5,7 +5,7 @@
         <dashboard-card :title="header">
           <template v-slot:default>
             <v-row align="center" justify="start" class="pa-12">
-              <v-col cols="12" md="6" lg="4" v-for="(data, index) in dataTypes" :key="`data-${index}`">
+              <v-col cols="12" md="6" xl="4" v-for="(data, index) in dataTypes" :key="`data-${index}`">
                 <horizontal-card
                   :title="data.title"
                   :updated-at="data.updated_at"
@@ -23,25 +23,36 @@
         </dashboard-card>
         <dashboard-card :title="apiHeader" class="mt-12">
           <template v-slot:default>
-            <v-row align="center" justify="center">
-              <v-col cols="3" v-if="$vuetify.breakpoint.smAndUp">
-                <v-subheader>키프리스 API</v-subheader>
+            <v-row align="center" justify="start" class="pa-12">
+              <v-col cols="12">
+                <p class="mb-0 font-weight-bold headline">키프리스 API</p>
               </v-col>
-              <v-col cols="8" sm="7">
+              <v-col cols="2" v-if="$vuetify.breakpoint.smAndUp">
+                <v-subheader>KEY</v-subheader>
+              </v-col>
+              <v-col cols="12" sm="10">
+                <p class="mb-0">{{apiKey.kipris}}</p>
+              </v-col>
+              <v-col cols="2" v-if="$vuetify.breakpoint.smAndUp">
+                <v-subheader>업데이트</v-subheader>
+              </v-col>
+              <v-col cols="8">
                 <v-text-field
-                  v-model="apiKey.kipris"
+                  v-model="newKiprisApiKey"
                   label="KIPRIS"
                   outlined
                   :rules="[v=>!!v || '값을 입력해주세요.']"
-                  hide-details
+                  class="pt-8"
                 />
               </v-col>
-              <v-col cols="4" sm="2" md="1">
+              <v-col cols="3" sm="2">
                 <v-btn
                   class="mr-2 pa-1 font-weight-bold"
-                  @click="updateApiKey('kipris')"
+                  @click="updateApiKey"
                   x-large
                   solo
+                  :color="baseColor"
+                  dark
                 >
                   변경
                 </v-btn>
@@ -57,109 +68,112 @@
 <script>
 import DashboardCard from "@/components/Cards/DashboardCard";
 import HorizontalCard from "@/components/Cards/HorizontalCard";
+import {mapState} from "vuex";
 
 export default {
   name: "index",
   components: {DashboardCard, HorizontalCard},
-  asyncData({store}) {
-    return store.dispatch('data/getPatentLog').then(
-      res => {
-        let data = [
-          {
-            key: 'CASH_FLOW',
-            title: '현금 흐름',
-            updated_at: res['cashFlow']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['cashFlow']['count']
-          },
-          {
-            key: 'IPC_TCT',
-            title: 'TCT 지수',
-            updated_at: res['tct']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['tct']['count']
-          },
-          {
-            key: 'ROYALTY_RATE',
-            title: '로열티율',
-            updated_at: res['royaltyRate']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['royaltyRate']['count']
-          },
-          {
-            key: 'INDUSTRY_ROYALTY_RATE',
-            title: '산업별 로열티율',
-            updated_at: res['industryRoyaltyRate']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['industryRoyaltyRate']['count']
-          },
-          {
-            key: 'INDUSTRY_TECH_ELEMENT',
-            title: '사업 기술 요소',
-            updated_at: res['industryTechElement']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['industryTechElement']['count']
-          },
-          {
-            key: 'INDUSTRY_PIONEER_AND_PERIOD',
-            title: '개척율 및 사업화 기간',
-            updated_at: res['industryPioneerAndPeriod']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['industryPioneerAndPeriod']['count']
-          },
-          {
-            key: 'DISCOUNT_RATE',
-            title: '할인율',
-            updated_at: res['discountRate']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['discountRate']['count']
-          },
-          {
-            key: 'INDUSTRY_DISCOUNT_RATE',
-            title: '산업별 할인율',
-            updated_at: res['industryDiscountRate']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['industryDiscountRate']['count']
-          },
-          {
-            key: 'RISK_PREMIUM',
-            title: '위험 프리미엄',
-            updated_at: res['riskPremium']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['riskPremium']['count']
-          },
-          {
-            key: 'PATENT_PQI_GRADE',
-            title: '특허 PQI 지수',
-            updated_at: res['patentPQIGrade']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['patentPQIGrade']['count']
-          },
-          {
-            key: 'ALL_INDUSTRY_SALES',
-            title: '전 손익계산서 매출액',
-            updated_at: res['allIndustrySales']['updatedAt'].replace('T', ' ').split('.')[0],
-            count: res['allIndustrySales']['count']
-          },
-        ]
-        let result = []
-        let colorList = store.getters.getColorList
-        for (let i = 0; i < data.length; i++) {
-          result.push({
-            ...data[i],
-            progress: 0,
-            color: colorList[i]
-          })
-        }
-        // TODO: API KEY 받아오기
-        return {
-          apiKey: {
-            kipris: ''
-          },
-          dataTypes: result,
-          fetchError: null
-        }
-      },
-      err => {
-        return {
-          dataTypes: [],
-          apiKey: {
-            kipris: ''
-          },
-          fetchError: err,
-        }
+  async asyncData({$axios, store}) {
+    try {
+      let dataInfo = await Promise.all([$axios.$get('/patent/file'), $axios.$get('/kipris/key')])
+      let fileLog = dataInfo[0]['patentFileLog']
+      let apiKey = dataInfo[1]['apiKey']
+
+      let data = [
+        {
+          key: 'CASH_FLOW',
+          title: '현금 흐름',
+          updated_at: fileLog['cashFlow']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['cashFlow']['count']
+        },
+        {
+          key: 'IPC_TCT',
+          title: 'TCT 지수',
+          updated_at: fileLog['tct']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['tct']['count']
+        },
+        {
+          key: 'ROYALTY_RATE',
+          title: '로열티율',
+          updated_at: fileLog['royaltyRate']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['royaltyRate']['count']
+        },
+        {
+          key: 'INDUSTRY_ROYALTY_RATE',
+          title: '산업별 로열티율',
+          updated_at: fileLog['industryRoyaltyRate']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['industryRoyaltyRate']['count']
+        },
+        {
+          key: 'INDUSTRY_TECH_ELEMENT',
+          title: '사업 기술 요소',
+          updated_at: fileLog['industryTechElement']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['industryTechElement']['count']
+        },
+        {
+          key: 'INDUSTRY_PIONEER_AND_PERIOD',
+          title: '개척율 및 사업화 기간',
+          updated_at: fileLog['industryPioneerAndPeriod']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['industryPioneerAndPeriod']['count']
+        },
+        {
+          key: 'DISCOUNT_RATE',
+          title: '할인율',
+          updated_at: fileLog['discountRate']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['discountRate']['count']
+        },
+        {
+          key: 'INDUSTRY_DISCOUNT_RATE',
+          title: '산업별 할인율',
+          updated_at: fileLog['industryDiscountRate']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['industryDiscountRate']['count']
+        },
+        {
+          key: 'RISK_PREMIUM',
+          title: '위험 프리미엄',
+          updated_at: fileLog['riskPremium']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['riskPremium']['count']
+        },
+        {
+          key: 'PATENT_PQI_GRADE',
+          title: '특허 PQI 지수',
+          updated_at: fileLog['patentPQIGrade']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['patentPQIGrade']['count']
+        },
+        {
+          key: 'ALL_INDUSTRY_SALES',
+          title: '전 손익계산서 매출액',
+          updated_at: fileLog['allIndustrySales']['updatedAt'].replace('T', ' ').split('.')[0],
+          count: fileLog['allIndustrySales']['count']
+        },
+      ]
+      let result = []
+      let colorList = store.getters.getColorList
+      for (let i = 0; i < data.length; i++) {
+        result.push({
+          ...data[i],
+          progress: 0,
+          color: colorList[i]
+        })
       }
-    )
+
+      return {
+        apiKey: {
+          kipris: apiKey['key']
+        },
+        dataTypes: result,
+        fetchError: null
+      }
+    }
+    catch (e) {
+      return {
+        dataTypes: [],
+        apiKey: {
+          kipris: null
+        },
+        fetchError: e,
+      }
+    }
   },
   created() {
     if (!!this.fetchError) {
@@ -174,9 +188,22 @@ export default {
     apiHeader: 'API KEY 관리',
     newDataFile: null,
     resetTime: 1000,
+    newKiprisApiKey: ''
   }),
+  computed: {
+    ...mapState({
+      baseColor: "baseColor"
+    })
+  },
   methods: {
     async uploadFile({key, file}, callback) {
+      if (!file) {
+        this.$notifier.showMessage({
+          content: '파일을 입력해주세요',
+          color: 'error'
+        })
+        return
+      }
       let index = this.dataTypes.findIndex(obj => obj.key === key)
 
       let formData = new FormData();
@@ -235,6 +262,7 @@ export default {
         responseType: "arraybuffer",
         onDownloadProgress: function (progressEvent) {
           this.dataTypes[index].progress = parseInt(progressEvent.total) <= 0 ? '' : parseInt(Math.round((progressEvent.loaded / progressEvent.total) * 100));
+          console.log(`loaded: ${progressEvent.loaded}`)
         }.bind(this)
       }
 
@@ -245,6 +273,7 @@ export default {
 
       await this.$store.dispatch('data/downloadFile', data).then(
         res => {
+          console.log('response!')
           let blob = new Blob([res], {type: "text/csv"});
           let objectUrl = URL.createObjectURL(blob);
           const link = document.createElement('a');
@@ -300,8 +329,31 @@ export default {
           return 'allIndustrySales'
       }
     },
-    updateApiKey(key) {
-      console.log(this.apiKey[`${key}`])
+    async updateApiKey() {
+      if (!this.newKiprisApiKey) {
+        this.$notifier.showMessage({
+          content: '값을 입력해주세요.',
+          color: 'error'
+        })
+        return
+      }
+      let newKey = this.newKiprisApiKey
+      await this.$store.dispatch('data/updateKiprisApiKey', {key: this.newKiprisApiKey}).then(
+        res => {
+          this.$notifier.showMessage({
+            content: '업데이트 완료',
+            color: 'success'
+          })
+          this.apiKey.kipris = newKey;
+          this.newKiprisApiKey = ''
+        },
+        err => {
+          this.$notifier.showMessage({
+            content: err,
+            color: 'error'
+          })
+        }
+      )
     }
   },
 }
--- store/data.js
+++ store/data.js
@@ -42,5 +42,27 @@ export const actions = {
           reject(err.response.data.message)
         })
     })
-  }
+  },
+  async getKiprisApiKey({commit}) {
+    return new Promise((resolve, reject) => {
+      this.$axios.$get('/kipris/key').then(
+        res => {
+          resolve(res['apiKey'])
+        })
+        .catch(err => {
+          reject(err.response.data.message)
+        })
+    })
+  },
+  async updateKiprisApiKey({commit}, payload) {
+    return new Promise((resolve, reject) => {
+      this.$axios.$put('/kipris/key', payload).then(
+        res => {
+          resolve(res)
+        })
+        .catch(err => {
+          reject(err.response.data.message)
+        })
+    })
+  },
 }
